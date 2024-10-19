19d3ed2b4b0eb7aae95f351b55849db1dab16eb5
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-10-19 02:34:54 +0000

    atv imp

---
+++ exe-marcio/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int[] vetor = new int[1000];
+        int num = ler.nextInt();
+
+        
+        ler.close();
+    }
+}
