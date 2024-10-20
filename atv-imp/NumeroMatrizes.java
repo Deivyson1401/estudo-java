60e7f3fecd42f8309a9a21058fff69d2df8b3bd5
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-10-20 04:20:51 +0000

    atv matri de numero

---
+++ atv-imp/NumeroMatrizes.java
@@ -0,0 +1,11 @@
+public class NumeroMatrizes {
+    public static void main(String[] args) {
+        imprimir("Olá, mundo!");
+    }
+
+    public static void imprimir(String texto){
+        for(int i =0;i<texto.length();i++){
+            System.out.println(texto.charAt(i));;
+        }
+    }
+}
